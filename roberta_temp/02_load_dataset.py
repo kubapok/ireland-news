7f405cdad4c5f6565d6f81445bfdc82b5550b68c
kubapok <jakubpokrywka@gmail.com>
2021-09-19 11:09:33 +0200

    roberta finetunning temp embedding 1 epoch

---
+++ roberta_temp/02_load_dataset.py
@@ -0,0 +1,34 @@
+import pickle
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from config import MODEL
+
+dataset = load_dataset('csv', sep='\t', data_files={'train': ['../train/huggingface_format_year.tsv'], 'test': ['../dev-0/huggingface_format_year.tsv']})
+test_dataset = load_dataset('csv', sep='\t', data_files='../test-A/huggingface_format_year.tsv')
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+test_tokenized_datasets = test_dataset.map(tokenize_function, batched=True)
+
+train_dataset = tokenized_datasets["train"].shuffle(seed=42)
+eval_dataset_full = tokenized_datasets["test"]
+eval_dataset_small = tokenized_datasets["test"].select(range(2000))
+test_dataset = test_tokenized_datasets["train"]
+
+with open('train_dataset.pickle','wb') as f_p:
+    pickle.dump(train_dataset, f_p)
+
+with open('eval_dataset_small.pickle','wb') as f_p:
+    pickle.dump(eval_dataset_small, f_p)
+
+with open('eval_dataset_full.pickle','wb') as f_p:
+    pickle.dump(eval_dataset_full, f_p)
+
+with open('test_dataset.pickle','wb') as f_p:
+    pickle.dump(test_dataset, f_p)
+     
+
---
+++ roberta_temp/03_train.py
@@ -0,0 +1,76 @@
+import pickle
+from config import LABELS_LIST, MODEL
+
+with open('train_dataset.pickle','rb') as f_p:
+    train_dataset = pickle.load(f_p)
+
+with open('eval_dataset_small.pickle','rb') as f_p:
+    eval_dataset_small = pickle.load(f_p)
+
+with open('eval_dataset_full.pickle','rb') as f_p:
+    eval_dataset_full = pickle.load(f_p)
+
+with open('test_dataset.pickle','rb') as f_p:
+    test_dataset = pickle.load(f_p)
+
+
+from transformers import AutoModelForSequenceClassification
+
+model = AutoModelForSequenceClassification.from_pretrained(MODEL, num_labels=7)
+
+from transformers import TrainingArguments
+
+
+training_args = TrainingArguments("test_trainer",
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=4,
+        evaluation_strategy='steps',
+        #eval_steps=2_000,
+        #save_steps=2_000,
+        eval_steps=20_000,
+        save_steps=20_000,
+        num_train_epochs=1,
+        gradient_accumulation_steps=2,
+        learning_rate = 1e-6,
+        #warmup_steps=4_000,
+        warmup_steps=4,
+        load_best_model_at_end=True,
+        )
+
+import numpy as np
+from datasets import load_metric
+
+metric = load_metric("accuracy")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
+from transformers import Trainer
+
+trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset_small,
+            compute_metrics=compute_metrics,
+            )
+
+#trainer.train(resume_from_checkpoint=True)
+trainer.train()
+trainer.save_model("./roberta-retrained")
+trainer.evaluate()
+
+
+eval_predictions = trainer.predict(eval_dataset_full).predictions.argmax(1)
+
+with open('../dev-0/out.tsv', 'w') as f_out:
+    for pred in eval_predictions:
+        f_out.write(LABELS_LIST[pred] + '\n')
+
+test_predictions = trainer.predict(test_dataset).predictions.argmax(1)
+with open('../test-A/out.tsv', 'w') as f_out:
+    for pred in test_predictions:
+        f_out.write(LABELS_LIST[pred] + '\n')
