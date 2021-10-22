fac97a487d06d9a7959f2532eef513a60de77f13
kubapok <jakubpokrywka@gmail.com>
2021-10-22 10:35:16 +0200

    roberta_year_as_text_better_finetunning_only_weekday

---
+++ roberta_year_as_text_better_finetunning_only_weekday/02_load_dataset.py
@@ -0,0 +1,47 @@
+import pickle
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from config import MODEL
+from tqdm import tqdm
+
+dataset = load_dataset('csv', sep='\t', data_files={'train': ['./train_huggingface_format.csv'], 'test': ['./dev-0_huggingface_format.csv']})
+test_dataset = load_dataset('csv', sep='\t', data_files='./test-A_huggingface_format.csv')
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+def tokenize_function(examples):
+    t = tokenizer(examples["text"], padding="max_length", truncation=True)
+    return t
+
+test_tokenized_datasets = test_dataset.map(tokenize_function, batched=True)
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+
+#for d in ('train', 'test'):
+#        for i in tqdm(range(len(tokenized_datasets[d]))):
+#            tokenized_datasets[d][i][column] = [tokenized_datasets[d][i][column] ] * 512 #len(tokenized_datasets[d][i]['input_ids'])
+#
+#d = 'train'
+#for column in tqdm(('date', 'day_of_month', 'day_of_year', 'month', 'year', 'year_cont')):
+#    for i in tqdm(range(len(test_tokenized_datasets[d]))):
+#        test_tokenized_datasets[d][i][column] = [test_tokenized_datasets[d][i][column] ] * 512 #len(test_tokenized_datasets[d][i]['input_ids'])
+
+train_dataset = tokenized_datasets["train"].shuffle(seed=42)
+eval_dataset_full = tokenized_datasets["test"]
+eval_dataset_small = tokenized_datasets["test"].select(range(2000))
+test_dataset = test_tokenized_datasets["train"]
+
+
+with open('train_dataset.pickle','wb') as f_p:
+    pickle.dump(train_dataset, f_p)
+
+with open('eval_dataset_small.pickle','wb') as f_p:
+    pickle.dump(eval_dataset_small, f_p)
+
+with open('eval_dataset_full.pickle','wb') as f_p:
+    pickle.dump(eval_dataset_full, f_p)
+
+with open('test_dataset.pickle','wb') as f_p:
+    pickle.dump(test_dataset, f_p)
+     
+
---
+++ roberta_year_as_text_better_finetunning_only_weekday/config.py
@@ -0,0 +1,18 @@
+LABELS_DICT = {'news':0,
+        'sport':1,
+        'business':2,
+        'opinion':3,
+        'culture':4,
+        'lifestyle':5,
+        'removed':6}
+
+
+LABELS_LIST = ['news',
+        'sport',
+        'business',
+        'opinion',
+        'culture',
+        'lifestyle',
+        'removed']
+
+MODEL = 'roberta-base'
